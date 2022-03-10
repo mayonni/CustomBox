b5d4b69adc483ced0c95f93ac1d499df6263e498
Sunofokkusu <justine.pruliere4@etu.univ-lorraine.fr>
2022-03-10 17:38:12 +0100

    ateliers

--- src/Vue/VuePrincipale.php
+++ src/Vue/VuePrincipale.php
@@ -107,8 +107,8 @@ class VuePrincipale{
                         <ul class='navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4'>
                         <li class='nav-item'><a class='nav-link active' href='#accueil'>Accueil</a></li>
                         <li class='nav-item'><a class='nav-link active' href='#atelier'>Ateliers</a></li>
+                         <li class='nav-item'><a class='nav-link active' href='#custom'>CustomBox</a></li>
                         <li class='nav-item'><a class='nav-link active' href='#prods'>Produits</a></li>
-                        <li class='nav-item'><a class='nav-link active' href='#custom'>CustomBox</a></li>
                         </ul>
                         <form class='d-flex'>
                             $connect
