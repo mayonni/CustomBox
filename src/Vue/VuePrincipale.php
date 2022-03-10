b6dcfab8450d8b7e5c0bdfbdb3c01375b08ac191
CHEVALEYRE Antoine <antoine.chevaleyre6@etu.univ-lorraine.fr>
2022-03-10 18:01:41 +0100

    ajout panier

--- src/Vue/VuePrincipale.php
+++ src/Vue/VuePrincipale.php
@@ -9,9 +9,10 @@ use \Slim\Container;
   define('SCRIPT_ROOT', 'http://localhost/CrazyCharlyDay/CustomBox/Ressources');
 class VuePrincipale{
 
+
     public array $tab;
     public Container $container;
-    public string  $fdg;
+
 
     public function __construct(array $tab, Container $container)
     {
@@ -36,16 +37,16 @@ class VuePrincipale{
                         <div class='card-body p-4'>
                             <div class='text-center'>
                             <!-- Product name-->
-                                <h5 class='fw-bolder text-dark'>$l[titre]</h5>
+                                <h5 class='fw-bolder text-dark titre'>$l[titre]</h5>
                                 <!-- Product weight-->
-                                  <p class='text-dark'>poids: $l[poids]</p>
-                                  <p class='text-dark'>catégorie: $l[categorie]</p>
+                                  <p>poids:<span class='text-dark poids'> $l[poids]</span></p>
+                                  <p class='text-dark categorie'>Atelier: $l[categorie]</p>
                                   <p class='text-dark'>$l[description]</p>
                             </div>
                         </div>
                         <!-- Product actions-->
                         <div class='card-footer p-4 pt-0 border-top-0 bg-transparent'>
-                            <div class='text-center'><a class='btn btn-outline-dark mt-auto' href='#'>Ajouter à ma box</a></div>
+                            <div class='text-center'><a class='btn btn-outline-dark mt-auto' id=$l[titre]>Ajouter à ma box</a></div>
                         </div>
                     </div>
                 </div>";
@@ -96,7 +97,9 @@ class VuePrincipale{
             <!-- Navigation-->
             <nav class='navbar navbar-expand-lg navbar-light bg-light fixed-top'>
                 <div class='container px-4 px-lg-5'>
+
                     <a class='navbar-brand' href='#!'>L'Atelier</a>
+
                     <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'><span class='navbar-toggler-icon'></span></button>
                     <div class='collapse navbar-collapse' id='navbarSupportedContent'>
                         <ul class='navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4'>
@@ -106,16 +109,37 @@ class VuePrincipale{
                         <li class='nav-item'><a class='nav-link active' href='#custom'>CustomBox</a></li>
                         </ul>
                         <form class='d-flex'>
+                            <div>
                             $connect
-                            <button class="btn btn-outline-dark" type="submit">
-                                <i class="bi-cart-fill me-1"></i>
+                            &emsp;
+                            <button id='cart' class="btn btn-outline-dark" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                                <i  class="bi-cart-fill me-1"></i>
                                 Ma box
-                                <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                                <span id='nombre-produit' class="badge bg-dark text-white ms-1 rounded-pill"></span>
                             </button>
+                            </div>
                         </form>
                     </div>
                 </div>
             </nav>
+
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                                          <div class='offcanvas-header'>
+                                            <h5 id="offcanvasRightLabel">Contenu de votre box</h5>
+                                            <button type="button" class='btn-close text-reset' data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                          </div>
+                                          <div style='position:relative;'>
+                                          <div class='offcanvas-body' id='contenu-panier'>
+
+
+                                          </div>
+                                          <div style='position:absolute; bottom:0;'>
+                                          &emsp;
+
+                                          </div>
+                                          </div>
+                                        </div>
+
         <!-- Icon -->
         <section class='bg-dark py-5' id='accueil'>
                     <div class='container px-4 px-lg-5 my-5'>
@@ -178,7 +202,7 @@ class VuePrincipale{
         <!-- Bootstrap core JS-->
         <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js'></script>
         <!-- Core theme JS-->
-        <script src='js/scripts.js'></script>
+        <script src='$root/js/scripts.js'></script>
     </body>
 </html>
 
